c5e86c0001fecb3c1a479fa4637f8b34962cdfec
mohmoh <mohmohtheinzaw379@gmail.com>
2023-03-12 19:51:34 +0630

    add user & usermanager model setup

---
+++ src/model/user.ts
@@ -0,0 +1,42 @@
+import mongojs from "mongojs"
+import config from "../config"
+import DatabaseQuery from "../database/databaseQuery"
+const userCollection = "users"
+class User{
+    userId:string
+    constructor(userId:string){
+        this.userId = userId
+    
+    }
+    async getInfo(){
+        let queryField = {_id:mongojs.ObjectId(this.userId)}
+        try {
+            let data = await DatabaseQuery.getOneDocument(config.dbName,userCollection,queryField)
+            return data
+        } catch (error) {
+            throw error
+        }
+    }
+
+    async updateInfo(updateData:any){
+        let queryField = {_id:mongojs.ObjectId(this.userId)}
+        let updateField = {$set:{updateData}}
+        try {
+            let data = await DatabaseQuery.updateDocument(config.dbName,userCollection,queryField,updateField)
+            return data
+        } catch (error) {
+            throw error
+        }
+    }
+
+    async delet(){
+        let queryField = {_id:mongojs.ObjectId(this.userId)}
+        try {
+            let data = await DatabaseQuery.deleteDocument(config.dbName,userCollection,queryField)
+            return data
+        } catch (error) {
+            throw error
+        }
+    }
+}
+export default User
---
+++ src/model/userManager.ts
@@ -0,0 +1,42 @@
+import mongojs from "mongojs"
+import DatabaseQuery from "../database/databaseQuery"
+import config from "../config"
+const userCollection = "users"
+class UserManager {
+    async createUser(userData:any){
+        try {
+            let data = await DatabaseQuery.createDocument(config.dbName,userCollection,userData)
+            return data
+        } catch (error) {
+            throw error
+        }
+    }
+
+    async totalCount(){
+        try {
+            let data = await DatabaseQuery.getTotalDocuments(config.dbName,userCollection)
+            return data
+        } catch (error) {
+            throw error
+        }
+    }
+
+    async getUserWithRange(skip:number,limit:number){
+        try {
+            let data = await DatabaseQuery.getDocumentsWithSkipAndLimit(config.dbName,userCollection,skip,limit)
+            return data
+        } catch (error) {
+            throw error
+        }
+    }
+
+    async searchUser(search:string){
+        let searchField = {$text:{$search:search}}
+        try {
+            let data = await DatabaseQuery.getAllDocuments(config.dbName,userCollection,searchField)
+            return data
+        } catch (error) {
+            throw error
+        }
+    }
+}
